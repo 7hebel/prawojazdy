80438e2bf118535f0c289f246cabd8b2000be517
gental-py <a43735150@gmail.com>
2025-10-29 14:18:08 +0100

    Start work on backend

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,15 +2,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./app";
 import UiTest from "./ui/UiTest";
+import { Quiz } from "./ui/Quiz";
 import './index.css';
 
 const root = document.getElementById("root");
+
+
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/ui-test" element={<UiTest />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
   </BrowserRouter>,
 );
